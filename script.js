977cb9681b447566c99a4d50731d33231f7373f6
grupo006efg <164170558+grupo006efg@users.noreply.github.com>
2024-10-02 08:55:48 -0300

    correcao task

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let tasks =['comprar leite','Ler um livro','Estudar JavaScript'];
 //Função para atualizar a lista de tarefas na página
 function updateTaskList(){
     const taskList = document.getElementById('taskList');
-    tasks.forEach(tasks=> {
+    tasks.forEach(task => {
         const li =document.createElement('li');
         li.textContent = task;
         taskList.appendChild(li);
