85045e0c0589cad4fd75d29cc34a474237af1db6
grupo006efg <164170558+grupo006efg@users.noreply.github.com>
2024-10-02 08:48:41 -0300

    updateTaskList

---
+++ script.js
@@ -0,0 +1,14 @@
+//Array de tarefas
+let tasks =['comprar leite','Ler um livro','Estudar JavaScript'];
+
+//Função para atualizar a lista de tarefas na página
+function updateTaskList(){
+    const taskList = document.getElementById('taskList');
+    tasks.forEach(tasks=> {
+        const li =document.createElement('li');
+        li.textContent = task;
+        taskList.appendChild(li);
+    
+        
+    });
+}
